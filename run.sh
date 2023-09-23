58de6f980b8884946cd3c74ccce23ddc1a2b282d
K Tact <ktact.dev@gmail.com>
2023-09-23 13:07:40 +0900

    Kernel bootup successful!

--- run.sh
+++ run.sh
@@ -4,5 +4,15 @@ set -xue
 # Filepath of QEMU
 QEMU=qemu-system-riscv32
 
+# Path of clang
+CC=$(which clang)
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
