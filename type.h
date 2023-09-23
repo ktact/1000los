4e0399413ae87fb00c3f37d25c50b306ecab9872
K Tact <ktact.dev@gmail.com>
2023-09-23 22:44:10 +0900

    Implement utility functions(memxxx, strxxx)

--- type.h
+++ type.h
@@ -3,3 +3,11 @@
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
+typedef uint32_t paddr_t;
+typedef uint32_t vaddr_t;
+
+typedef int bool;
+#define true  1
+#define false 0
+
+#define NULL ((void *) 0)
